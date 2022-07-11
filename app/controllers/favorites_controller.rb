a476c87240219feb12b714838965c1d338503f50
EC2 Default User <ec2-user@ip-172-31-15-51.ap-northeast-1.compute.internal>
2022-07-11 13:15:09 +0000

    favorite-Asynchronous

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
     
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)#こう記述することで、カレントユーザに関連したFavoriteクラスの新しいインスタンスが作成可能。
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)#こう記述することで、カレントユーザに関連したFavoriteクラスの新しいインスタンスが作成可能。
     favorite.save
-    redirect_to books_path
+    # redirect_to books_path
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to books_path
+    # redirect_to books_path
   end
 
 end
